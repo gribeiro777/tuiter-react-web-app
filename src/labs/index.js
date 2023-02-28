dac451c042f5643f0d536becc17b492466f087f6
gribeiro777 <ribeiro.ga@husky.neu.edu>
2023-02-27 20:30:40 -0500

    a6 react.js sp23

---
+++ src/labs/index.js
@@ -0,0 +1,15 @@
+import {Link}
+    from "react-router-dom";
+import Nav from "../nav";
+import Assignment6 from "./a6";
+
+function Labs() {
+    return(
+        <div>
+            <Nav/>
+            <Assignment6/>
+        </div>
+    )
+}
+export default Labs;
+
